bf8e21cee11888604eb321a0561225edc6c8d274
royce79-creator <connorsboyce07@gmail.com>
2021-04-15 15:10:16 -0700

    made changes to the the script.

--- javascript.js
+++ javascript.js
@@ -1,17 +1,34 @@
-var today = new Date();
-var hourNow = today.getHours();
-var greeting;
+// //This will prompt a greeting depdning on waht time 
+// of day it is. 
+function userGreeting() {
+    var today = new Date();
+    var hourNow = today.getHours();
+    var greeting;
+    if (hourNow > 18) {
+        greeting = 'Good evening Adventurer'; 
+    }  else if (hourNow > 12) {
+        greeting = 'Good afternoon Adventurer';
+    }  else if (hourNow > 0) {
+        greeting = 'Good Morning Adventurer';  
+    }  else {
+        greeting = 'Welcome Adventurer';
+    }
+    return'<h3>' + greeting + '</h3>'; }
 
-if (hourNow > 18) {
-    greeting = 'Good evening!'; 
-}  else if (hourNow > 12) {
-    greeting = 'Good afternoon!';
-}  else if (hourNow > 0) {
-    greeting = 'Good Morning!';  
-}  else {
-    greeting = 'Welcome!';
+function personHere () {
+    console.log (personHere)
+    var person = prompt("what is your name?");
+
+    return '<h3>' + person + "!" + '</h3>'; }
+
+function ask() {
+    var answer=prompt("Do you like the city or nature?");
+    if (answer=="nature") {
+        alert("We have some great hiking spots to show you!");
+        return; 
+}   if (answer=="city"){
+        alert("We have some great spots in the Seattle greater area to show you!");
+        return; 
 }
-
-document.write('<h3>' + greeting + '</h3>');
-
+}
 
